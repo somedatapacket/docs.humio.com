c91078677f27b8f4d02eaee4562404d898db6633
Thomas Anagrius <thomas@humio.com>
2018-09-19 12:16:52 +0200

    Remove developer specific script

--- run-hugo-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run --rm --name "docs.humio.com" -p 1313:1313 \
-  --volume $(pwd):/src \
-  --volume /tmp/hugo-build-output:/output \
-  --env HUGO_WATCH=true \
-  --env ARGS="--disableFastRender" \
-  jojomi/hugo
